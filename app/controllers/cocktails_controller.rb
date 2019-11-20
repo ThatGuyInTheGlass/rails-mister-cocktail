eef891b4062e366c48946cd10bd34bec040efad5
Ross <ross.a@protonmail.ch>
2019-11-20 15:17:08 +0200

    Time to work

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,49 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def edit
+  end
+
+  def create
+    @cocktail = cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Your cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to @cocktail, notice: 'Your Cocktail was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktail_url, notice: 'We removed your cocktail from the database.'
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
