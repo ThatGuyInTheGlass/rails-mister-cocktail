d6321c4461d13b4174ebfef766e36d16a990cd3e
Ross <ross.a@protonmail.ch>
2019-11-20 13:21:36 +0200

    After seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,21 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+Ingredient.destroy_all
+puts "Removed the ingredients from the list"
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+string = open(url).read
+hash = JSON.parse(string)
+
+array = hash['drinks']
+array.each do |minihash|
+  ingredient = minihash['strIngredient1']
+  Ingredient.create(name:ingredient)
+  puts "Added the #{ingredient} ingredient"
+end
+
